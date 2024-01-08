520dd8493e63caac252fd11a4cfc55e874bc1e76
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-08 21:58:47 +0100

    feat: add confirm modal and add to library handle

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,10 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom';
+import { useRef } from 'react';
+import Modal from '../components/Modal';
+import { useUser } from '../context/UserProvider';
+
+type ModalHandles = {
+    open: () => void;
+    close: () => void;
+};
 
 export default function Checkout() {
-    return <div>
-        <button>Confirm</button>
-        <Link to={'..'}>
-            <button>Cancel</button>
-        </Link>
-    </div>
+    const {user} = useUser()
+    const modalRef = useRef<ModalHandles>();
+    const params = useParams();
+    async function handleBuy(event: React.MouseEvent<HTMLElement>) {
+        event.preventDefault();
+        try {
+            const response = await fetch(
+                'http://localhost:3000/games/' + params.gameId + '/buy',
+                {
+                    method: 'post',
+                    headers: {
+                        'Application-type': 'application/json',
+                        Authorization:
+                            'Bearer ' + sessionStorage.getItem('token'),
+                    },
+                }
+            );
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            }
+            const resData = await response.json();
+            console.log(resData);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <>
+            <Modal
+                ref={modalRef}
+                title="Confirm purchase"
+                handleYes={handleBuy}
+            >
+                Are you sure?
+            </Modal>
+            <div>
+                <h1>Checkout</h1>
+                <div>
+                    <select name="payment" id="payment">
+                        <option value="balance">Balance</option>
+                        <option value="creditCard">Credit Card</option>
+                    </select>
+                </div>
+                <p>Your new </p>
+                <div>
+                    <button
+                        type="button"
+                        onClick={() => modalRef.current?.open()}
+                    >
+                        Confirm
+                    </button>
+                    <Link to={'..'}>
+                        <button>Cancel</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
