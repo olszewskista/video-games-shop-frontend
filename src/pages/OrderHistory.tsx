414f15d0a169cc73c92ce0673d7cfd8bb166879e
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-09 21:10:09 +0100

    feat: add feedback modal after refund

--- src/pages/OrderHistory.tsx
+++ src/pages/OrderHistory.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import FeedbackModal from '../components/FeedbackModal';
 import useFetch from '../hooks/useFetch';
 
 type Order = {
@@ -15,7 +17,14 @@ type Order = {
     refundable: boolean;
 };
 
+type ModalHandles = {
+    open: () => void;
+    close: () => void;
+};
+
 export default function OrderHistoryPage() {
+    const positiveModal = useRef<ModalHandles>();
+    const negativeModal = useRef<ModalHandles>();
     const { data, isLoading } = useFetch(
         [],
         'http://localhost:3000/user/orders'
@@ -38,46 +47,71 @@ export default function OrderHistoryPage() {
             }
             const resData = await response.json();
             console.log(resData);
+            positiveModal.current?.open();
         } catch (error) {
+            negativeModal.current?.open();
             console.log(error);
         }
     }
-    console.log(data);
     return (
-        <div>
-            {isLoading && <p>Loading...</p>}
-            {data && (
-                <ul className="flex flex-col gap-4 p-8 bg-neutral-800 rounded-xl">
-                    {data.map((order: Order) => (
-                        <li key={order._id} className="flex justify-between items-center flex-wrap">
-                            <div
-                                className={
-                                    'flex flex-wrap gap-4 items-center py-2 px-4 rounded ' +
-                                    (order.type === 'purchase'
-                                        ? 'bg-red-500/50'
-                                        : 'bg-green-500/50')
-                                }
+        <>
+            <FeedbackModal
+                title="Success"
+                content="Your order has been refunded"
+                isPositive
+                ref={positiveModal}
+                onClick={() => window.location.reload()}
+            />
+            <FeedbackModal
+                title="Error"
+                content="Something went wrong"
+                isPositive={false}
+                ref={negativeModal}
+            />
+            <div className="p-8 bg-neutral-800 rounded-xl">
+                {isLoading && <p>Loading...</p>}
+                {data?.length === 0 && <p>No orders</p>}
+                {data && (
+                    <ul className="flex flex-col gap-4">
+                        {data.map((order: Order) => (
+                            <li
+                                key={order._id}
+                                className="flex justify-between items-center flex-wrap"
                             >
-                                <p>ID: {order._id}</p>
-                                <img
-                                    src={order.game.image}
-                                    alt={order.game.title}
-                                    className="w-12"
-                                />
-                                <p>{order.game.title}</p>
-                                <p>
-                                    {order.type === 'purchase' ? '-' : '+'}
-                                    {order.price}$
-                                </p>
-                                <p>{new Date(order.date).toUTCString()}</p>
-                            </div>
-                            {order.refundable && <button onClick={() => handleRefund(order._id)} className='bg-neutral-700 rounded px-4 py-2 h-fit'>
-                                Refund
-                            </button>}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+                                <div
+                                    className={
+                                        'flex flex-wrap gap-4 items-center py-2 px-4 rounded ' +
+                                        (order.type === 'purchase'
+                                            ? 'bg-red-500/50'
+                                            : 'bg-green-500/50')
+                                    }
+                                >
+                                    <p>ID: {order._id}</p>
+                                    <img
+                                        src={order.game.image}
+                                        alt={order.game.title}
+                                        className="w-12"
+                                    />
+                                    <p>{order.game.title}</p>
+                                    <p>
+                                        {order.type === 'purchase' ? '-' : '+'}
+                                        {order.price}$
+                                    </p>
+                                    <p>{new Date(order.date).toUTCString()}</p>
+                                </div>
+                                {order.refundable && (
+                                    <button
+                                        onClick={() => handleRefund(order._id)}
+                                        className="bg-neutral-700 rounded px-4 py-2 h-fit"
+                                    >
+                                        Refund
+                                    </button>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </>
     );
 }
