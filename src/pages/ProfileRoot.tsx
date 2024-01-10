649e490b8d9e61e4c625f198743cfe191d0c1f21
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-10 23:20:50 +0100

    feat: add admin tools to profile navbar

--- src/pages/ProfileRoot.tsx
+++ src/pages/ProfileRoot.tsx
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink, Outlet } from 'react-router-dom';
+import { useUser } from '../context/UserProvider';
 
-const liClasses = 'mx-8 text-xl mb-2'
+const liClasses = 'mx-8 text-xl mb-6 uppercase font-bold w-fit'
 
 export default function ProfileRootLayout() {
+    const {user} = useUser()
     return (
         <div className="flex gap-4">
             <ul className="md:w-max w-1/4 bg-neutral-800 h-[90vh] rounded-br-3xl pt-8">
@@ -11,7 +13,7 @@ export default function ProfileRootLayout() {
                     <NavLink
                         to={'/profile'}
                         className={({ isActive }) =>
-                            isActive ? 'text-blue-500 underline' : ''
+                            isActive ? 'text-blue-500 ' : 'py-2'
                         }
                         end
                     >
@@ -23,24 +25,35 @@ export default function ProfileRootLayout() {
                     <NavLink
                         to={'/profile/orders'}
                         className={({ isActive }) =>
-                            isActive ? 'text-blue-500 underline' : ''
+                            isActive ? 'text-blue-500 ' : 'py-2'
                         }
                     >
                         <FontAwesomeIcon icon={'shopping-bag'} className='mr-2'/>
-                        Order history
+                        Order History
                     </NavLink>
                 </li>
                 <li className={liClasses}>
                     <NavLink
                         to={'/profile/support'}
                         className={({ isActive }) =>
-                            isActive ? 'text-blue-500 underline' : ''
+                            isActive ? 'text-blue-500 ' : 'py-2'
                         }
                     >
                         <FontAwesomeIcon icon={'headset'} className='mr-2'/>
                         Support
                     </NavLink>
                 </li>
+                {user?.isAdmin && <li className={liClasses}>
+                    <NavLink
+                        to={'/profile/admin'}
+                        className={({ isActive }) =>
+                            isActive ? 'text-blue-500 ' : 'py-2'
+                        }
+                    >
+                        <FontAwesomeIcon icon={'toolbox'} className='mr-2'/>
+                        Admin Tools
+                    </NavLink>
+                </li>}
             </ul>
             <div className="flex-1 m-4">
                 <Outlet />
