4212b675fd88498f0ec3f78b7b6400dec31ac5c9
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-12 02:31:40 +0100

    refactor: swich to useFetch hook instead of useState/useEffect

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -2,32 +2,18 @@ import FakeGamesList from '../components/FakeGamesList';
 import FavoriteGames from '../components/FavoriteGames';
 import GamesFilterOptions from '../components/GamesFilterOptions';
 import GamesList from '../components/GamesList';
-import { useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
 export default function StorePage() {
-    const [games, setGames] = useState(null)
-    useEffect(() => {
-        async function fetchGames() {
-            const response = await fetch('http://localhost:3000/games')
-
-            if (!response.ok) {
-                console.log('error')
-                return
-            }
-
-            const resData = await response.json()
-            setGames(resData)
-        }
-        fetchGames()
-    }, [])
+    const {data, isLoading, setData, error} = useFetch(null, 'http://localhost:3000/games')
     return (
         <>
-            <GamesFilterOptions setGames={setGames}/>
-            <FakeGamesList />
-            {games && <FavoriteGames games={games}/>}
+            <GamesFilterOptions setGames={setData}/>
+            {data && <FavoriteGames games={data}/>}
             <h1 className='text-3xl text-white text-center mb-4 uppercase font-bold'>Shop</h1>
-            {games && <GamesList games={games} />}
-            {!games && <div>Loading games...</div>}
+            {isLoading && <FakeGamesList />}
+            {data && <GamesList games={data} />}
+            {error && <div className='text-white text-center text-2xl'>Something went wrong!</div>}
         </>
     );
 }
